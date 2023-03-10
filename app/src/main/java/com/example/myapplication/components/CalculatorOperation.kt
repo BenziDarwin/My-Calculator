54e01027965d6ddd86249fe17003480bf161fc62
Ssali Benjamin <ssalibenjamin0402@gmail.com>
2023-03-10 10:41:20 +0300

    First commit

---
+++ app/src/main/java/com/example/myapplication/components/CalculatorOperation.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.components
+
+sealed class CalculatorOperation(val symbol: String) {
+    object Add: CalculatorOperation("+")
+    object Subtract: CalculatorOperation("-")
+    object Divide: CalculatorOperation("/")
+    object Multiply: CalculatorOperation("x")
+
+}
